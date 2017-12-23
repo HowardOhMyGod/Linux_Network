ff9d0b312a70e76dbe4c70283248dc5bf28408af
Howard Ho <howard036060006@gmail.com>
2017-12-23 21:49:43 +0800

    finish part3

--- SP_HW9/part3/lookup9.c
+++ SP_HW9/part3/lookup9.c
@@ -8,26 +8,58 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <string.h>
 #include "dict.h"
 
 int lookup(Dictrec * sought, const char * resource) {
-	static int sockfd;
-	static struct sockaddr_in server;
+	static struct sockaddr_in server, client;
+	static int sockfd, namesize;
 	struct hostent *host;
 	static int first_time = 1;
+	Dictrec temp;
 
 	if (first_time) {  /* Set up server address & create local UDP socket */
 		first_time = 0;
 
-		/* Set up destination address.
-		 * Fill in code. */
+		/* Set up destination address. */
+		memset((char *)&server, '\0', sizeof(server));
+		server.sin_family = AF_INET;
+		server.sin_port = PORT;
+
+		if((host = gethostbyname(resource)) == NULL){
+			perror("gethostbyname");
+			exit(1);
+		}
+
+		memcpy((char *) &server.sin_addr, host -> h_addr, host -> h_length);
+
+		/*set client address*/
+		memset((char *)&client, '\0', sizeof(client));
+		client.sin_family = AF_INET;
+		client.sin_port = htons(0);
+		client.sin_addr.s_addr = htonl(INADDR_ANY);
+
 
 		/* Allocate a socket.
 		 * Fill in code. */
+	 sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	 if(sockfd < 0){
+			perror("socket");
+			exit(1);
+		}
+
+		if(bind(sockfd, (struct sockaddr*) &client, sizeof(client)) == -1){
+			perror("bind");
+			exit(1);
+		}
 	}
 
 	/* Send a datagram & await reply
 	 * Fill in code. */
+	strcpy(temp.word, sought -> word);
+	sendto(sockfd, &temp, sizeof(Dictrec), 0, (struct sockaddr*)&server, sizeof(server));
+	recvfrom(sockfd, &temp, sizeof(Dictrec), 0, (struct sockaddr*)&server, &namesize);
+	strcpy(sought -> text, temp.text);
 
 	if (strcmp(sought->text,"XXXX") != 0) {
 		return FOUND;
--- SP_HW9/part3/udp_server.c
+++ SP_HW9/part3/udp_server.c
@@ -9,13 +9,14 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <errno.h>
-
+#include <string.h>
 #include "dict.h"
 
 int main(int argc, char **argv) {
 	static struct sockaddr_in server,client;
-	int sockfd,siz;
-	Dictrec dr, *tryit = &dr;
+	int sockfd, rval,clen = sizeof(client);
+	struct hostent *host;
+	Dictrec tryit;
 
 	if (argc != 2) {
 		fprintf(stderr,"Usage : %s <datafile>\n",argv[0]);
@@ -24,34 +25,66 @@ int main(int argc, char **argv) {
 
 	/* Create a UDP socket.
 	 * Fill in code. */
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd < 0){
+		perror("socket");
+		exit(1);
+	}
 
 	/* Initialize address.
 	 * Fill in code. */
+	memset((char *)&server, '\0', sizeof(server));
+ 	server.sin_family = AF_INET;
+ 	server.sin_port = PORT;
+
+ 	if((host = gethostbyname("localhost")) == NULL){
+ 		perror("gethostbyname");
+ 		exit(1);
+ 	}
 
 	/* Name and activate the socket.
 	 * Fill in code. */
+	 memcpy((char *) &server.sin_addr, host -> h_addr, host -> h_length);
+
+ 	/*bind*/
+ 	if(bind(sockfd, (struct sockaddr*) &server, sizeof(server))){
+ 		perror("bind");
+ 		exit(1);
+ 	}
 
 	for (;;) { /* await client packet; respond immediately */
 
-		siz = sizeof(client); /* siz must be non-zero */
-
 		/* Wait for a request.
 		 * Fill in code. */
-
-		while (___) {
+		rval = recvfrom(sockfd, &tryit, sizeof(Dictrec), 0, (struct sockaddr *)&client, &clen);
+		
+		while (rval > 0 || rval >= sizeof(Dictrec)) {
 			/* Lookup request and respond to user. */
-			switch(lookup(tryit,argv[1]) ) {
-				case FOUND: 
+			switch(lookup(&tryit,argv[1])) {
+				case FOUND:
 					/* Send response.
 					 * Fill in code. */
+
+					rval = sendto(sockfd, &tryit, sizeof(Dictrec), 0, (struct sockaddr *)&client, clen);
+ 					if (rval < 0){
+ 						perror("send");
+ 					}
 					break;
-				case NOTFOUND : 
+				case NOTFOUND :
 					/* Send response.
 					 * Fill in code. */
+					strcpy(tryit.text, "XXXX");
+					rval = sendto(sockfd, &tryit, sizeof(Dictrec), 0, (struct sockaddr *)&client, clen);
+ 					if (rval < 0){
+ 						perror("send");
+ 					}
 					break;
 				case UNAVAIL:
 					DIE(argv[1]);
 			} /* end lookup switch */
+
+				rval = recvfrom(sockfd, &tryit, sizeof(Dictrec), 0, (struct sockaddr *)&client, &clen);
+
 		} /* end while */
 	} /* end forever loop */
 } /* end main */
