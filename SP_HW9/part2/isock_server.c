dfaef23d71e5d44728ab07eaddabd745f758a520
Howard Ho <howard036060006@gmail.com>
2017-12-23 20:22:04 +0800

    finish part2

--- SP_HW9/part2/isock_server.c
+++ SP_HW9/part2/isock_server.c
@@ -10,12 +10,14 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <string.h>
 
 #include "dict.h"
 
 int main(int argc, char **argv) {
-	static struct sockaddr_in server;
-	int sd,cd,n;
+	static struct sockaddr_in server, client;
+	struct hostent *host;
+	int sd,cd,rval, clen = sizeof(client);
 	Dictrec tryit;
 
 	if (argc != 2) {
@@ -25,40 +27,86 @@ int main(int argc, char **argv) {
 
 	/* Create the socket.
 	 * Fill in code. */
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sd < 0){
+		perror("socket");
+		exit(1);
+	}
 
 	/* Initialize address.
 	 * Fill in code. */
+	memset((char *)&server, '\0', sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = PORT;
+
+	if((host = gethostbyname("localhost")) == NULL){
+		perror("gethostbyname");
+		exit(1);
+	}
 
 	/* Name and activate the socket.
 	 * Fill in code. */
+	/* set addr*/
+	memcpy((char *) &server.sin_addr, host -> h_addr, host -> h_length);
 
+	/*bind*/
+	if(bind(sd, (struct sockaddr*) &server, sizeof(server))){
+		perror("bind");
+		exit(1);
+	}
+
+	/*listen*/
+	if(listen(sd, 5* sizeof(Dictrec))){
+		perror("listen");
+		exit(1);
+	}
 	/* main loop : accept connection; fork a child to have dialogue */
 	for (;;) {
 
 		/* Wait for a connection.
 		 * Fill in code. */
+		 cd = accept(sd, (struct sockaddr *)&client, &clen);
+		if(cd == -1){
+			perror("accept");
+			exit(1);
+		}
 
 		/* Handle new client in a subprocess. */
 		switch (fork()) {
-			case -1 : 
+			case -1 :
 				DIE("fork");
 			case 0 :
 				close (sd);	/* Rendezvous socket is for parent only. */
 				/* Get next request.
 				 * Fill in code. */
-				while (____) {
+				rval = recv(cd, &tryit, sizeof(Dictrec), 0);
+				while (rval > 0 || rval >= sizeof(Dictrec)) {
 					/* Lookup the word , handling the different cases appropriately */
 					switch(lookup(&tryit,argv[1]) ) {
 						/* Write response back to the client. */
+
 						case FOUND:
 							/* Fill in code. */
+							rval = send(cd, &tryit, sizeof(Dictrec), 0);
+              if (rval < 0){
+                perror("send");
+              }
 							break;
 						case NOTFOUND:
 							/* Fill in code. */
-							 break;
+							strcpy(tryit.text, "XXXX");
+              rval = send(cd, &tryit, sizeof(Dictrec), 0);
+              if (rval < 0){
+                perror("send");
+              }
+						 break;
 						case UNAVAIL:
 							DIE(argv[1]);
 					} /* end lookup switch */
+
+					/* get new request*/
+					rval = recv(cd, &tryit, sizeof(Dictrec), 0);
+
 				} /* end of client dialog */
 				exit(0); /* child does not want to be a parent */
 
--- SP_HW9/part2/lookup8.c
+++ SP_HW9/part2/lookup8.c
@@ -8,31 +8,54 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <string.h>
 
 #include "dict.h"
 
 int lookup(Dictrec * sought, const char * resource) {
-	static int sockfd;
+	static int sd;
 	static struct sockaddr_in server;
 	struct hostent *host;
 	static int first_time = 1;
+	Dictrec temp;
 
 	if (first_time) {        /* connect to socket ; resource is server name */
 		first_time = 0;
 
 		/* Set up destination address. */
+		memset((char *)&server, '\0', sizeof(server));
 		server.sin_family = AF_INET;
-		/* Fill in code. */
+		server.sin_port = PORT;
+
+		if((host = gethostbyname(resource)) == NULL){
+			perror("gethostbyname");
+			exit(1);
+		}
+
+		memcpy((char *) &server.sin_addr, host -> h_addr, host -> h_length);
 
 		/* Allocate socket.
 		 * Fill in code. */
+		sd = socket(AF_INET, SOCK_STREAM, 0);
+ 		if(sd < 0){
+ 			perror("socket");
+ 			exit(1);
+ 		}
 
 		/* Connect to the server.
 		 * Fill in code. */
+		if(connect(sd, (struct sockaddr *) &server, sizeof(server))){
+			perror("connect");
+			exit(1);
+		}
 	}
 
 	/* write query on socket ; await reply
 	 * Fill in code. */
+	strcpy(temp.word, sought -> word);
+ 	send(sd, &temp, sizeof(Dictrec), 0);
+ 	recv(sd, &temp, sizeof(Dictrec), 0);
+ 	strcpy(sought -> text, temp.text);
 
 	if (strcmp(sought->text,"XXXX") != 0) {
 		return FOUND;
